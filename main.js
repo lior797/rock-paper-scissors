8f7227d3cb91984a172e4f778b8b82ce22de11c5
lior797 <lior797@gmail.com>
2023-05-06 19:54:39 +0300

    Add scores and functions fullGame and winner
    Add playerScore and computerScore variables to keep score of the game.
    Add relevant score each time within the gameRound function.
    Make fullGame function that loops for five times to make five rounds of game, keeping score of each round.
    Make winner function that calculates who is the winner at the end of the fifth round and declares it.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   let randomIndex = Math.floor(Math.random() * choices.length);
@@ -23,12 +26,52 @@ function gameRound() {
     (playerChoice == "paper" && computerChoice == "rock") ||
     (playerChoice == "scissors" && computerChoice == "paper")
   ) {
+    playerScore++;
     return "You win! " + playerChoice + " beats " + computerChoice;
   } else if (playerChoice == computerChoice) {
     return "It's a tie, you both chose " + playerChoice;
   } else {
+    computerScore++;
     return "You lose! " + computerChoice + " beats " + playerChoice;
   }
 }
 
-console.log(gameRound());
+function winner() {
+  if (playerScore > computerScore) {
+    console.log("This is the final score.");
+    console.log("You are the WINNNER!!! Congratulations!");
+  } else if (playerScore < computerScore) {
+    console.log("This is the final score.");
+    console.log("You lost to the computer, maybe next time you will succeed!");
+  } else {
+    console.log("The final score is a tie. Well done");
+  }
+}
+
+function fullGame() {
+  playerScore = 0;
+  computerScore = 0;
+  for (i = 0; i < 5; i++) {
+    console.log(gameRound());
+    if (playerScore > computerScore) {
+      console.log(
+        "The updated score is " + playerScore + ":" + computerScore + " to you!"
+      );
+    } else if (playerScore < computerScore) {
+      console.log(
+        "The updated score is " +
+          computerScore +
+          ":" +
+          playerScore +
+          " to computer!"
+      );
+    } else {
+      console.log(
+        "The updated score is a tie " + playerScore + ":" + computerScore
+      );
+    }
+  }
+  winner();
+}
+
+fullGame();
